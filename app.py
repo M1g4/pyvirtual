2e7779b8b53158e3dbafd181950366ea57092327
Sebastian Lammel <sebastianlammel@googlemail.com>
2014-12-20 18:53:39 +0100

    added readme

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 from flask import Flask
 
-app = Flask (__name__)
+app = Flask(__name__)
+
+app.config["DEBUG"] = True
 
 @app.route("/")
 @app.route("/hello")
 def hello_world():
-	return "Hello World!"
+	return "Hello World!?!?!?"
 
 @app.route("/integer/<int:value>")
 def int_type(value):
 	print value + 1
 	return "Correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value * 2
+	return Correct
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "Correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello, {}".format(name), 200
+	else:
+		return "Not found", 404
 
 if __name__ == "__main__":
 	app.run()
